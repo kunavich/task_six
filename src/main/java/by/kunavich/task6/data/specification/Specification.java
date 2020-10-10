abe105e3863f145d4ccf6229238e95a384643733
kunavich <kunpash@gmail.com>
2020-10-10 09:52:38 +0300

    need to creater pull reqvest

--- src/main/java/by/kunavich/task6/data/specification/Specification.java
+++
@@ -1,9 +0,0 @@
-package by.kunavich.task6.data.specification;
-
-import by.kunavich.task6.model.Book;
-
-import java.util.List;
-
-public interface Specification<T> {
-    public Book find(List<Book> books , String value);
-}
